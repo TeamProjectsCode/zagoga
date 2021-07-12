a7bb8739e9fb060a2509d09dcbc9d5861235bb80
hyun <gbl1001@naver.com>
2021-07-12 20:09:16 +0900

    images

--- src/main/java/com/javalec/project_zagoga/controller/GhouseController.java
+++ src/main/java/com/javalec/project_zagoga/controller/GhouseController.java
@@ -59,7 +59,7 @@ public class GhouseController {
 	public String insert(Ghouse ghouse, @RequestParam("files") MultipartFile file, @PathVariable("h_no") int h_no)
 			throws IOException {
 
-		String fileAddr = "D:\\_Proni\\Intelli J\\project_zagoga\\src\\main\\resources\\static\\gh_image\\";
+		String fileAddr = "C:\\Users\\yeon\\IdeaProjects\\project_zagoga\\src\\main\\resources\\static\\gh_image\\";
 
 
 		String imageName = String.valueOf(System.currentTimeMillis());
